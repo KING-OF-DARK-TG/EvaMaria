b0b79fc5cc27c476769cce1d7d1d57d110bc8df6
KING-OF-DARK-TG <93397793+KING-OF-DARK-TG@users.noreply.github.com>
2021-10-31 22:12:15 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -1,4 +1,5 @@
 from pyrogram import Client, filters
+import random
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.errors.exceptions.bad_request_400 import MessageTooLong, PeerIdInvalid
 from info import ADMINS, LOG_CHANNEL, SUPPORT_CHAT
