395dbc2cce9f750fa167c5335639fc6125a7c3f3
KING-OF-DARK-TG <93397793+KING-OF-DARK-TG@users.noreply.github.com>
2021-10-31 19:08:02 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -91,7 +91,7 @@ async def answer(bot, query):
 def get_reply_markup(query):
     buttons = [
         [
-            InlineKeyboardButton('Search again', switch_inline_query_current_chat=query)
+            InlineKeyboardButton('🔄𝑺𝒆𝒂𝒓𝒄𝒉 𝑨𝒈𝒂𝒊𝒏🔄', switch_inline_query_current_chat=query)
         ]
         ]
     return InlineKeyboardMarkup(buttons)
